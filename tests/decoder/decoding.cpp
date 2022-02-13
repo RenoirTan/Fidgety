ecc13d52313101b573683201cea92d9a6163b08c
RenoirTan <renoirtan2005@gmail.com>
2022-02-13 17:14:54 +0800

    main: SPENT 1 HOUR TRYING TO FIX A BLOODY IO BUG

--- tests/decoder/decoding.cpp
+++ tests/decoder/decoding.cpp
@@ -4,6 +4,7 @@
 #else
 #   include <unistd.h>
 #endif
+#include <iostream>
 #include <string>
 #include <fidgety/decoder.hpp>
 #include <gtest/gtest.h>
@@ -19,6 +20,15 @@ std::string present_working_directory(void) {
     return pwd;
 }
 
+nlohmann::json load_json_from_file(const std::string &path) {
+    spdlog::trace("opening json file '{0}'", path);
+    std::ifstream handle(path, std::ifstream::in);
+    spdlog::debug("is json file open? {0}", handle.is_open());
+    auto object = nlohmann::json::parse(handle);
+    handle.close();
+    return object;
+}
+
 TEST(DecoderDecoding, DumpConf) {
     std::string pwd = present_working_directory();
     spdlog::set_level(spdlog::level::trace);
@@ -30,4 +40,13 @@ TEST(DecoderDecoding, DumpConf) {
     EXPECT_TRUE(decoder.isIntermediateOpened());
     DecoderStatus status = decoder.dumpToIntermediate();
     EXPECT_EQ(status, DecoderStatus::Ok);
+    decoder.closeConf();
+    decoder.closeIntermediate();
+    spdlog::debug("opening answer_key");
+    nlohmann::json answer_key = load_json_from_file(
+        "../../../resources/tests/decoder/test_0_answer.json"
+    );
+    spdlog::debug("opening my_answer");
+    nlohmann::json my_answer = load_json_from_file("../../../tmp/tests/decoder/test_0.json");
+    EXPECT_EQ(my_answer, answer_key);
 }
