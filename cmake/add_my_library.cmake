fc2c98803b0ce7540f617fcbed30ff05904d8c47
RenoirTan <renoirtan2005@gmail.com>
2022-02-20 15:50:00 +0800

    main: NormalConfEncoder and NormalConfDecoder are now considered extensions and more CMake variables have been added. The README has been updated to reflect such changes.

--- cmake/add_my_library.cmake
+++ cmake/add_my_library.cmake
@@ -46,6 +46,30 @@ macro(fidgety_install_library)
     endif()
 endmacro()
 
+macro(fidgety_install_extension_library)
+# ARGV0: name of the library
+    # ARGV1: name of the CMake file
+    if(ARGC LESS 2)
+        message(
+            FATAL_ERROR
+            "you need to provide the name of the library and the name its CMake file"
+        )
+    endif()
+    if(FIDGETY_MASTER_PROJECT)
+        install(
+            TARGETS ${ARGV0}
+            EXPORT ${ARGV0}
+            DESTINATION ${FIDGETY_EXPORT_LIB_DIR}
+        )
+        install(
+            EXPORT ${ARGV0}
+            DESTINATION ${FIDGETY_EXPORT_CMAKE_DIR}
+            NAMESPACE Fidgety::
+            FILE ${ARGV1}
+        )
+    endif()
+endmacro()
+
 macro(fidgety_link_test_libraries)
     # ARGV0: name of the test
     if(ARGC LESS 1)
