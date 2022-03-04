6d702703c6b7dade1a755a323562b142fefe51ed
RenoirTan <renoirtan2005@gmail.com>
2022-03-04 20:38:52 +0800

    main: implement OptionException

--- src/options/options.cpp
+++ src/options/options.cpp
@@ -8,4 +8,22 @@
  * @copyright Copyright (c) 2022
  */
 
+#include "spdlog/spdlog.h"
 #include <fidgety/options.hpp>
+#include <fidgety/_utils.hpp>
+
+using namespace Fidgety;
+
+std::string OptionException::codeAsErrorType(void) const {
+    switch (mCode) {
+        case 0: return "Ok";
+        case 1: return "InvalidValueType";
+        case 2: return "IncompatibleOptionEditor";
+        case 3: return "NotFound";
+        default: return "Other";
+    }
+}
+
+const char *OptionException::getSimpleWhat(void) const {
+    return "A Fidgety::OptionException occurred.";
+}
