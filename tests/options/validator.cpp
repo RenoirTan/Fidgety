041d05623e45901d64c56c0da16c4b8bfab1eefc
RenoirTan <renoirtan2005@gmail.com>
2022-03-13 10:31:05 +0800

    main: add tests for Validator

---
+++ tests/options/validator.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <fidgety/options.hpp>
+#include <gtest/gtest.h>
+#include "spdlog/spdlog.h"
+#include "dummies.hpp"
+
+using namespace Fidgety;
+
+TEST(OptionsValidator, Validator) {
+    Validator validator;
+    OptionEditor editor(OptionEditorType::Blanked, std::map<std::string, std::string>());
+    OptionValue value("value");
+    Option option("dummy", std::move(editor), std::move(validator), std::move(value));
+
+    ValidatorContextInner vci = makeValidatorContextInner(10);
+    ValidatorContext context(std::move(vci));
+    
+    ValidatorMessage message = option.validate(context);
+    EXPECT_EQ(message.fullMessage(), "Valid: Ok");
+}
